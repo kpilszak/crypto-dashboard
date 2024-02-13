e88b08ac587ac04c55298aa49217fdce23d80ae9
kpilszak <klaudia.pilszak@gmail.com>
2024-02-13 17:36:08 -0500

    Add news feed fetching

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,4 +1,30 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 const NewsFeed = () => {
+    const [articles, setArticles] = useState(null)
+
+    useEffect(() => {
+
+        const options = {
+            method: 'GET',
+            url: 'https://crypto-news-live9.p.rapidapi.com/news/CryptoNews',
+            headers: {
+                'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+                'X-RapidAPI-Host': 'crypto-news-live9.p.rapidapi.com'
+            }
+        }
+
+        axios.request(options).then((response) => {
+            console.log(response.data)
+            setArticles(response.data)
+        }).catch ((error) => {
+            console.error(error)
+        })
+    }, [])
+
+    console.log(articles)
+
     return (
         <div className="news-feed">
             NewsFeed
