d675c6576365e906c4b984651668f6077ad188e2
kpilszak <klaudia.pilszak@gmail.com>
2024-02-20 16:25:54 -0500

    Add /news endpoint for returning crypto news

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ require('dotenv').config()
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json('hi')
+})
+
+app.get('/news', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://crypto-news-live9.p.rapidapi.com/news/CryptoNews',
+        headers: {
+            'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+            'X-RapidAPI-Host': 'crypto-news-live9.p.rapidapi.com'
+        }
+    }
+
+    axios.request(options).then((response) => {
+        res.json(response.data)
+    }).catch ((error) => {
+        console.error(error)
+    })
+})
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
