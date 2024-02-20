a1c8604669763eaa7e7253b2bad89fd7b29a966d
kpilszak <klaudia.pilszak@gmail.com>
2024-02-20 16:15:38 -0500

    Server listens on 8000

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
 require('dotenv').config()
+
+const app = express()
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
